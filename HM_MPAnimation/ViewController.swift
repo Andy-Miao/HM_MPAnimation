2d2c16111a272ef56c2ef44c643e075f907f9371
Andyhu <871649716qq.com>
2019-07-10 20:48:46 +0800

    Initial Commit

---
+++ HM_MPAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HM_MPAnimation
+//
+//  Created by 胡苗 on 2019/7/10.
+//  Copyright © 2019 胡苗. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HM_MPAnimationTests/HM_MPAnimationTests.swift
@@ -0,0 +1,34 @@
+//
+//  HM_MPAnimationTests.swift
+//  HM_MPAnimationTests
+//
+//  Created by 胡苗 on 2019/7/10.
+//  Copyright © 2019 胡苗. All rights reserved.
+//
+
+import XCTest
+@testable import HM_MPAnimation
+
+class HM_MPAnimationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
